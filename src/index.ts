d5d0a136313b159ad41995e1dc19688cc5f598ee
AtHeartEngineer <atheartengineer@gmail.com>
2023-08-01 19:12:08 -0400

    made server params optional

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,8 @@ export type IdentityCommitmentT = bigint;
 
 export enum RoomType {
   PUBLIC = 'public',
-  GATED = 'gated',
   PRIVATE = 'private',
-  SECURE = 'secure'
+  PIXEL = 'pixel'
 }
 
 export interface BandadaGroupI {
@@ -15,6 +14,7 @@ export interface BandadaGroupI {
 }
 
 export interface MembershipI {
+  type?: RoomMembershipType;
   identityCommitments?: IdentityCommitmentT[];
   rlnContract?: RLNContractI;
   bandadaGroup?: BandadaGroupI;
@@ -41,11 +41,18 @@ export interface SystemMessageI {
   room?: RLNFullProof['rlnIdentifier']; // optionally send it to one room or all rooms
 }
 
+enum RoomMembershipType {
+  IDENTITY_LIST = 'IDENTITY_LIST',
+  RLN_CONTRACT = 'RLN_CONTRACT',
+  BANDADA = 'BANDADA'
+}
+
 export interface RoomI {
   id: string; // database ID
   roomId: RLNFullProof['rlnIdentifier'] | string; // RLN Identifier
   name: string; // Readable name
   rateLimit?: number; // Milliseconds between epochs
+  banRateLimit?: number; // starting number of epochs banned for
   userMessageLimit?: number; // default number of messages per epoch per user
   membership?: MembershipI; // List of Identity Commitments, or a contract address for an RLN contract
   type?: RoomType; // Public or private, if undefinied, assume public
@@ -58,8 +65,8 @@ export interface ServerI {
   id: bigint;
   name: string;
   version?: string;
-  serverInfoEndpoint: string;
+  serverInfoEndpoint?: string;
   messageHandlerSocket?: string; // Default port for websocket connections
   rooms?: RoomI[];
-  selectedRoom?: RoomI['id'];
+  selectedRoom?: RoomI['roomId'];
 }
