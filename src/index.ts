5efa469c442318f62bf63b9b54e482f87838ae52
AtHeartEngineer <atheartengineer@gmail.com>
2023-08-10 16:35:00 -0400

    removed all enums because prisma can't handle enums properly

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,6 @@ import type { RLNFullProof } from 'rlnjs';
 export { str2BigInt, genId, randomBigInt } from './utils';
 export type IdentityCommitmentT = bigint | string;
 
-export enum RoomType {
-  PUBLIC = 'PUBLIC',
-  PRIVATE = 'PRIVATE',
-  PIXEL = 'PIXEL'
-}
-
 export interface BandadaGroupI {
   groupID: string;
   url: string;
@@ -33,12 +27,6 @@ export interface SystemMessageI {
   room?: RLNFullProof['rlnIdentifier']; // optionally send it to one room or all rooms
 }
 
-export enum RoomMembershipType {
-  IDENTITY_LIST = 'IDENTITY_LIST',
-  RLN_CONTRACT = 'RLN_CONTRACT',
-  BANDADA = 'BANDADA'
-}
-
 export interface RoomI {
   id: string; // database ID
   roomId: RLNFullProof['rlnIdentifier'] | string; // RLN Identifier
@@ -46,14 +34,14 @@ export interface RoomI {
   rateLimit?: number; // Milliseconds between epochs
   banRateLimit?: number; // starting number of epochs banned for
   userMessageLimit?: number; // default number of messages per epoch per user
-  membershipType?: RoomMembershipType | string;
+  membershipType?: string;
   identities?: IdentityCommitmentT[];
   contractAddress?: RLNContractI;
   bandadaAddress?: BandadaGroupI;
   epochs?: any[]; // this is for use in the db, not for the client
   messages?: MessageI[]; // this is a list of messages DATABASE REFERENCES to messages
   claimCodes?: string[]; // this is a list of claim codes for the room
-  type?: RoomType; // Public or private, if undefinied, assume public
+  type?: string; // Public or private, if undefinied, assume public
 }
 
 export interface ServerI {
